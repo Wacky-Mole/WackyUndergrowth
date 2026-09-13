6a2c5aaefcf259cf4f6f813ae03d87ec3b2a915b
WackyMole <wacky@wackymole.com>
2026-09-13 11:06:58 -0500

    1.0.1

--- Plugin.cs
+++ Plugin.cs
@@ -16,7 +16,7 @@ namespace WackyUndergrowth
     public class WackyUndergrowthPlugin : BaseUnityPlugin
     {
         internal const string ModName = "WackyUndergrowth";
-        internal const string ModVersion = "1.0.0";
+        internal const string ModVersion = "1.0.1";
         internal const string Author = "WackyMole";
         private const string ModGUID = Author + "." + ModName;
         private static string ConfigFileName = ModGUID + ".cfg";
--- Thunderstore/CHANGELOG.md
+++ Thunderstore/CHANGELOG.md
@@ -1,3 +1,4 @@
 | `Version` | `Update Notes`    |
 |-----------|-------------------|
 | 1.0.0     | - Initial Release |
+| 1.0.1     | - Update for Deep North |
